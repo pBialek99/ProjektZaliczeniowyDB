5db3796876222877479c56b74078d55af735c148
Paweł Białek <pawel99bialek@gmail.com>
2024-12-30 17:39:55 +0100

    Surowy projekt - utworzony

---
+++ Sklep/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sklep.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
